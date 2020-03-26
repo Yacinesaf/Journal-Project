354adbc49cf3e11ab0947588a731434d2a3d6ea2
Yacinesaf <yacinesaf96@gmail.com>
2020-03-26 15:41:49 -0400

    cleaned the code

--- src/reduxStore/store.js
+++ src/reduxStore/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers/index'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 
-const intialState = {}
-const middlewares = [thunk]
+const intialState = {};
+const middlewares = [thunk];
 
-const store = createStore(rootReducer, intialState, applyMiddleware(...middlewares))
+const store = createStore(rootReducer, intialState, applyMiddleware(...middlewares));
 
 
-export default store
+export default store;
