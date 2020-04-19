18e1b940b27a472c0c60e4f80ce83c2616bfd854
Yacinesaf <yacinesaf96@gmail.com>
2020-04-18 22:05:06 -0400

    entry content is responsive and added a back button to it

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,42 +1,52 @@
 import React from 'react';
-import { Typography, Grid, IconButton } from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
 import '../style.css'
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 function Entry({ title, body, reading }) {
   const history = useHistory()
   const theme = useTheme();
-  const xsOnly = useMediaQuery(theme.breakpoints.only('xs'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
     <div>
-      <IconButton  onClick={()=> history.push('/entries')}>
-        <ArrowBackIosIcon fontSize='large' style={{color : 'rgb(82, 84, 170)'}} />
-      </IconButton>
-      <Grid className='appBackground' container justify='center' alignContent='flex-start' style={{ minHeight: 'calc(100vh - 64px)', backgroundColor: '#f2f2f2' }}>
-        <Grid item xs={10} >
-          <Typography variant='h3' style={{ fontWeight: 550, textAlign: 'left', paddingTop: 100 }} color='initial'>{title}</Typography>
+      <ArrowBackIosIcon
+        onClick={() => history.push('/entries')}
+        fontSize='large'
+        style={{
+          color: 'rgb(82, 84, 170)',
+          paddingLeft: 25,
+          position: "absolute",
+          top: mdDown ? 30 : 100,
+          cursor: 'pointer'
+        }} />
+      <Grid className='appBackground' container justify='center' alignContent='flex-start' style={{ minHeight: '100vh', backgroundColor: '#f2f2f2' }}>
+        <Grid item xs={11} md={10} >
+          <Typography variant={mdDown ? 'h4' : 'h3'} style={{ fontWeight: 550, textAlign: mdDown ? 'center' : 'left', paddingTop: 130 }} color='initial'>{title}</Typography>
         </Grid>
         <Grid item xs={12}>
-          <Grid container justify='center' alignContent='center' style={{ paddingTop: 100 }}>
-            <Grid item xs={5} style={{ paddingRight: 50 }}>
-              <Typography variant='h4' style={{ textAlign: 'left' }} >{body}</Typography>
+          <Grid container justify='center' alignContent='center' style={{ paddingTop: 50 }}>
+            <Grid item xs={11} sm={10} lg={5} style={{ paddingRight: mdDown ? 0 :  50 }}>
+              <Typography variant={mdDown ? 'h5' : 'h4'} style={{ textAlign: mdDown ? 'center' : 'left' }} >{body}</Typography>
             </Grid>
-            <Grid item xs={5}>
-              <div className='floating' style={{
-                position: "absolute",
-                backgroundImage: `url(${reading})`,
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain',
-                height: 500,
-                width: 700,
-                float: 'right'
-              }} />
-            </Grid>
+            {mdDown ? null
+              : <Grid item xs={5}>
+                <div className='floating' style={{
+                  position: "fixed",
+                  backgroundImage: `url(${reading})`,
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'contain',
+                  height: 500,
+                  width: 600,
+                  right: 130,
+                  top: 200,
+                  float: 'right'
+                }} />
+              </Grid>}
           </Grid>
         </Grid>
       </Grid>
